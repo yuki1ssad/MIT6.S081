2fd7920e7b531549bc3c8cb14878fa6505f8856b
wang xuefei <18800205069@163.com>
2023-11-27 22:47:37 +0800

    lab1-primes ok

---
+++ user/primes.c
@@ -0,0 +1,38 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    int fd[2];
+    if (pipe(fd) == -1) {
+        fprintf(2, "pipe failed!");
+        exit(1);
+    }
+
+    int primes[] = {2,3,5,7,11,13,17,19,23,29,31};
+    int pid=0;
+    int i = 0;
+    int cnt = 10;
+
+    fprintf(2, "prime %d\n", primes[i]);
+    while(i < 10) {
+        pid = fork();
+        if (pid == 0) {
+            if (read(fd[0], &i, sizeof(int))) {
+                fprintf(2, "prime %d\n", primes[i]);
+                if (i == cnt) {
+                    close(fd[1]);
+                    exit(0);
+                } else {
+                    break;
+                }
+            }
+        } else {
+            i += 1;
+            write(fd[1], &i, sizeof(int));
+            wait(0);
+        }
+    }
+    
+    exit(0);
+}
